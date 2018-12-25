0acc80448f62f3f6363f3ff6bbfc00e557cc20a0
Sergii Kabashniuk <skabashniuk@redhat.com>
2018-12-25 16:54:02 +0200

    Scripts update

--- broadway-docker/1.0.0/build.sh
+++ broadway-docker/1.0.0/build.sh
@@ -4,10 +4,23 @@ set -u
 
 
 dockerImageName=ksmster/che-editor-$(yq r meta.yaml name)
+echo "Building docker image $dockerImageName"
 docker build . -t ${dockerImageName}
-sdocker push ${dockerImageName}
-if [ -f che-editor-plugin.tar.gz ]; then
+docker push ${dockerImageName}
+
+set +e
+git diff -s --exit-code  che-plugin.yaml
+CHE_PLUGIN_CHANGED=$?
+set -e
+if [[ $CHE_PLUGIN_CHANGED -eq 1 ]]; then
+   echo "che-plugin.yaml  changed repackaging";
+   if [ -f che-editor-plugin.tar.gz ]; then
     rm che-editor-plugin.tar.gz
+   fi
+   tar zcf che-editor-plugin.tar.gz che-plugin.yaml
+   git add che-plugin.yaml
+   git add che-editor-plugin.tar.gz
+   git commit -m "Update changes in che-plugin.yaml"
+   git push
 fi
 
-tar zcf che-editor-plugin.tar.gz che-plugin.yaml
--- broadway-docker/1.0.0/update-workspace.sh
+++ broadway-docker/1.0.0/update-workspace.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 set -e
 set -u
-
-CHE_HOST=$(oc get -o json  routes/che -o=go-template='{{ .spec.host }}')
+oc login -u developer -p developer
+CHE_HOST=$(oc get -o json --namespace  eclipse-che  routes/che -o=go-template='{{ .spec.host }}')
+echo "Creaing workspace in $CHE_HOST"
 curl -X POST --data-binary @.devfile -H "Content-type: text/x-yaml" http://$CHE_HOST/api/devfile
