caa04105153cd7098988e93970105b384daf0bfe
Sergii Kabashniuk <skabashniuk@redhat.com>
2019-01-06 15:11:38 +0200

    Create script to submit all workspaces
    Signed-off-by: Sergii Kabashniuk <skabashniuk@redhat.com>

---
+++ create-all.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+set -e
+set -u
+
+
+oc login -u developer -p developer >> /dev/null 
+CHE_HOST=$(oc get -o json --namespace  eclipse-che  routes/che -o=go-template='{{ .spec.host }}')
+echo "Using Che api server ${CHE_HOST}"
+find . -name ".devfile" \
+   -exec echo 'Creating workspace with devfile {}' \; \
+   -exec curl --silent --output /dev/null -X POST --data-binary @{} -H "Content-type: text/x-yaml" http://$CHE_HOST/api/devfile \;
+
+  
